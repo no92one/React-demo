ff238897687a8231ce8e9db6365678c1adbf89ba
unknown <linus.lindroth.92@gmail.com>
2026-02-12 10:28:47 +0100

    cleaning

--- src/PeopleCard.tsx
+++ src/PeopleCard.tsx
@@ -2,7 +2,7 @@ import type Person from "./Person.ts";
 
 export default function PeopleCard({ person }: { person: Person }) {
     const { name, age, hobby }: { name: string, age: number, hobby: string } = person
-    console.log(`person - `, person);
+    console.log("person - ", person);
     return <>
         <h1>{name} - {age}</h1>
         <p>Jag gillar {hobby} </p>
